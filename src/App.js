8fed3f7ac1f40a7e46e9589b5c1fe1dcdc1f230b
MaxwellPereira <maxwell.tj@hotmail.com>
2020-05-30 22:56:21 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meu nome é Maxwell Silva  !
+          My name is Maxwell Silva !
         </p>
         <a
           className="App-link"
